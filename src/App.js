f4154c5e8dadf6f2ba8c41151168e9adc9a41fe9
KarlGilden <karl.gildenhuys@gmail.com>
2021-11-17 11:39:30 +1300

    add private routing

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
 import Menubar from './components/Menubar';
 import PostView from './pages/PostView';
 import AddPostPage from './pages/AddPostPage';
+import PrivateRoute from './components/PrivateRoute';
 function App() {
   return (
     <>
@@ -17,7 +17,14 @@ function App() {
             <Route path="/" element={<HomePage/>}/>
             <Route path="/login" element={<LoginPage/>}/>
             <Route path="/post/:id" element={<PostView/>}/>
-            <Route path="/add-post" element={<AddPostPage/>}/>
+            <Route
+            path="/add-post"
+            element={
+              <PrivateRoute>
+                <AddPostPage />
+              </PrivateRoute>
+          }
+        />
           </Routes>
         </AuthProvider>
       </Router>
---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+import { Route, Navigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext'
+
+
+function PrivateRoute({ children }) {
+    const { user } = useAuth()
+    return user ? children : <Navigate to="/login" />;
+  }
+
+export default PrivateRoute;
